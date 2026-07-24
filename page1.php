e0eb8ca19ecb4df6d8fc706d93fe801ec1c2d0b3
Soujiro Seta <56367843+fiyugun@users.noreply.github.com>
2026-07-24 14:19:36 -0500

    Pagina de Php1
    se ingresa la primera pagina de php

---
+++ page1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Saludo en PHP</title>
+</head>
+<body>
+
+<?php
+    // Mensaje de saludo
+    echo "<h1>¡Hola, bienvenido a mi página PHP en Azure!</h1>";
+    echo "<h2>Estamos Agregando otra Linea, Una Linea Mas.</h2>"
+?>
+
+</body>
+</html>
